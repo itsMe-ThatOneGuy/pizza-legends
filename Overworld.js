6b5d19489084d96b2dffd434f18920f977f765bc
Matthew Smart <matthewsmart13@gmail.com>
2021-10-12 14:58:18 -0500

    finished video 3

--- Overworld.js
+++ Overworld.js
@@ -3,50 +3,38 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => {
+
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            //Draw Lower layer
+            this.map.drawLowerImage(this.ctx);
+
+            //Draw Game Objects
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.x += 0.02;
+                object.sprite.draw(this.ctx);
+            })
+
+            //Draw Upper layer
+            this.map.drawUpperImage(this.ctx);
+
+            requestAnimationFrame(() => {
+                step();
+            })
+        }
+        step();
     }
 
     init() {
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0)
-        };
-        image.src = "/images/maps/DemoLower.png"
-    
-        const x = 5;
-        const y = 6;
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen)
 
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0, //Left Cut
-                0, //Top cut
-                32,//width of cut
-                32,//height of cut
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-            );
-        };
-        shadow.src = "/images/characters/shadow.png"
-
-        const hero = new Image(); 
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0, //Left Cut
-                0, //Top cut
-                32,//width of cut
-                32,//height of cut
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-            );
-        };
-        hero.src = "/images/characters/people/hero.png"
-
+        this.startGameLoop();
+        
     }
 
 }
